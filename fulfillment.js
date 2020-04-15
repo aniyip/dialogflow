3dca75ce6dab21efe2c5c807d0607fd72c960c1e
aniyip <40328793+aniyip@users.noreply.github.com>
2020-04-15 07:15:20 -0400

    added SubmitTicket intent-related functions
    integrating BigQuery and function ticketCollection(agent)

--- fulfillment.js
+++ fulfillment.js
@@ -5,7 +5,12 @@
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
- 
+const BIGQUERY = require("@google-cloud/bigquery");
+
+const BIGQUERY_CLIENT = new BIGQUERY({
+projectId: "team-9-nbc" 
+});
+
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
  
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
@@ -61,6 +66,66 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     }
   }
 
+   function ticketCollection(agent) {
+    // Capture Parameters from the Current Dialogflow Context
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+    const OUTPUT_CONTEXTS = request.body.queryResult.outputContexts;
+    const EMAIL = OUTPUT_CONTEXTS[OUTPUT_CONTEXTS.length - 1].parameters["email.original"];
+    const ISSUE_CATEGORY = OUTPUT_CONTEXTS[OUTPUT_CONTEXTS.length - 1].parameters.category;
+    const ISSUE_TEXT = request.body.queryResult.queryText;
+ 
+
+    // The SQL Query to Run
+    const SQLQUERY = `WITH pred_table AS (SELECT 5 as seniority, "3-Advanced" as experience,
+          @category as category, "Request" as type)
+          SELECT cast(predicted_label as INT64) as predicted_label
+          FROM ML.PREDICT(MODEL helpdesk.predict_eta,  TABLE pred_table)`;
+
+    const OPTIONS = {
+      query: SQLQUERY,
+      // Location must match that of the dataset(s) referenced in the query.
+      location: "US",
+      params: {
+        category: ISSUE_CATEGORY
+      }
+    };
+    return BIGQUERY_CLIENT.query(OPTIONS)
+      .then(results => {
+        //Capture results from the Query
+        console.log(JSON.stringify(results[0]));
+        const QUERY_RESULT = results[0];
+        const ETA_PREDICTION = QUERY_RESULT[0].predicted_label;
+    
+        //Format the Output Message
+        agent.add( EMAIL + ", your ticket has been created. Someone will you contact shortly. " +
+            " The estimated response time is " + ETA_PREDICTION  + " days."
+        );
+        agent.add(
+          new Card({
+            title:
+              "New " + ISSUE_CATEGORY +
+              " Request for " + EMAIL +
+              " (Estimated Response Time: " + ETA_PREDICTION  +
+              " days)",
+            imageUrl:
+              "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+            text: "Issue description: " + ISSUE_TEXT,
+            buttonText: "Go to Ticket Record",
+            buttonUrl: "https://assistant.google.com/"
+          })
+        );
+        agent.setContext({
+          name: "submitticket-collectname-followup",
+          lifespan: 2
+        });
+      })
+      .catch(err => {
+        console.error("ERROR:", err);
+      });
+  }
+ 
+ 
   // // Uncomment and edit to make your own intent handler
   // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
   // // below to get this function to be run when a Dialogflow intent is matched
@@ -96,6 +161,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   intentMap.set('Default Fallback Intent', fallback);
   intentMap.set('Acronym Converter', acronymConverter);
   intentMap.set('Function Lookup', functionLookup);
+  intentMap.set("Submit Ticket - Issue Category", ticketCollection);
   // intentMap.set('your intent name here', yourFunctionHandler);
   // intentMap.set('your intent name here', googleAssistantHandler);
   agent.handleRequest(intentMap);
